bd6a574de1f098bf4778a2db436ac54662771279
一只小流浪 <109851917+YiZhiXiaoLiuLang@users.noreply.github.com>
2025-03-23 12:35:39 +0800

    Add files via upload

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -1,106 +1,126 @@
 <template>
-  <div
-    class="chapter-wrapper"
-    :style="bodyTheme"
-    :class="{ night: isNight, day: !isNight }"
-  >
-    <div class="tool-bar" :style="leftBarTheme">
-      <div class="tools">
-        <el-popover
-          placement="right"
-          :width="cataWidth"
-          trigger="click"
-          :visible-arrow="false"
-          v-model="popCataVisible"
-          popper-class="pop-cata"
-        >
-          <PopCata @getContent="getContent" ref="popCata" class="popup" />
+  <div>
+    <!-- 域名输入弹窗 -->
+    <el-dialog
+      title="请输入域名"
+      :visible.sync="domainDialogVisible"
+      width="30%"
+      :before-close="handleDomainDialogClose"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      :show-close="false"
+    >
+      <el-input style="display: none;" v-model="domain" placeholder="如有域名前缀，请输入（如 http://localhost:8088）。没有直接点确定"></el-input>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="confirmDomain">load</el-button>
+      </span>
+    </el-dialog>
 
+    <!-- 主内容区域 -->
+    <div
+      v-if="!domainDialogVisible"
+      class="chapter-wrapper"
+      :style="bodyTheme"
+      :class="{ night: isNight, day: !isNight }"
+    >
+      <div class="tool-bar" :style="leftBarTheme">
+        <div class="tools">
+          <el-popover
+            placement="right"
+            :width="cataWidth"
+            trigger="click"
+            :visible-arrow="false"
+            v-model="popCataVisible"
+            popper-class="pop-cata"
+          >
+            <PopCata @getContent="getContent" ref="popCata" class="popup" />
+
+            <div
+              class="tool-icon"
+              :class="{ 'no-point': noPoint }"
+              slot="reference"
+            >
+              <div class="iconfont">
+                &#58905;
+              </div>
+              <div class="icon-text">目录</div>
+            </div>
+          </el-popover>
+          <el-popover
+            placement="bottom-right"
+            width="470"
+            trigger="click"
+            :visible-arrow="false"
+            v-model="readSettingsVisible"
+            popper-class="pop-setting"
+          >
+            <ReadSettings class="popup" />
+
+            <div
+              class="tool-icon"
+              :class="{ 'no-point': noPoint }"
+              slot="reference"
+            >
+              <div class="iconfont">
+                &#58971;
+              </div>
+              <div class="icon-text">设置</div>
+            </div>
+          </el-popover>
+          <div class="tool-icon" @click="toShelf">
+            <div class="iconfont">
+              &#58892;
+            </div>
+            <div class="icon-text">书架</div>
+          </div>
+          <div class="tool-icon" :class="{ 'no-point': noPoint }" @click="toTop">
+            <div class="iconfont">
+              &#58914;
+            </div>
+            <div class="icon-text">顶部</div>
+          </div>
           <div
             class="tool-icon"
             :class="{ 'no-point': noPoint }"
-            slot="reference"
+            @click="toBottom"
           >
             <div class="iconfont">
-              &#58905;
+              &#58915;
             </div>
-            <div class="icon-text">目录</div>
+            <div class="icon-text">底部</div>
           </div>
-        </el-popover>
-        <el-popover
-          placement="bottom-right"
-          width="470"
-          trigger="click"
-          :visible-arrow="false"
-          v-model="readSettingsVisible"
-          popper-class="pop-setting"
-        >
-          <ReadSettings class="popup" />
-
+        </div>
+      </div>
+      <div class="read-bar" :style="rightBarTheme">
+        <div class="tools">
           <div
             class="tool-icon"
             :class="{ 'no-point': noPoint }"
-            slot="reference"
+            @click="toLastChapter"
           >
             <div class="iconfont">
-              &#58971;
+              &#58920;
             </div>
-            <div class="icon-text">设置</div>
           </div>
-        </el-popover>
-        <div class="tool-icon" @click="toShelf">
-          <div class="iconfont">
-            &#58892;
-          </div>
-          <div class="icon-text">书架</div>
-        </div>
-        <div class="tool-icon" :class="{ 'no-point': noPoint }" @click="toTop">
-          <div class="iconfont">
-            &#58914;
-          </div>
-          <div class="icon-text">顶部</div>
-        </div>
-        <div
-          class="tool-icon"
-          :class="{ 'no-point': noPoint }"
-          @click="toBottom"
-        >
-          <div class="iconfont">
-            &#58915;
-          </div>
-          <div class="icon-text">底部</div>
-        </div>
-      </div>
-    </div>
-    <div class="read-bar" :style="rightBarTheme">
-      <div class="tools">
-        <div
-          class="tool-icon"
-          :class="{ 'no-point': noPoint }"
-          @click="toLastChapter"
-        >
-          <div class="iconfont">
-            &#58920;
-          </div>
-        </div>
-        <div
-          class="tool-icon"
-          :class="{ 'no-point': noPoint }"
-          @click="toNextChapter"
-        >
-          <div class="iconfont">
-            &#58913;
+          <div
+            class="tool-icon"
+            :class="{ 'no-point': noPoint }"
+            @click="toNextChapter"
+          >
+            <div class="iconfont">
+              &#58913;
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="chapter-bar"></div>
-    <div class="chapter" ref="content" :style="chapterTheme">
-      <div class="content">
-        <div class="top-bar" ref="top"></div>
-        <div class="title" ref="title" v-if="show">{{ title }}</div>
-        <Pcontent :carray="content" />
-        <div class="bottom-bar" ref="bottom"></div>
+      <div class="chapter-bar"></div>
+      <div class="chapter" ref="content" :style="chapterTheme">
+        <div class="content">
+          <div class="top-bar" ref="top"></div>
+          <div class="title" ref="title" v-if="show">{{ title }}</div>
+          <Pcontent :carray="content" />
+          <div class="bottom-bar" ref="bottom"></div>
+        </div>
       </div>
     </div>
   </div>
@@ -113,137 +133,17 @@ import Pcontent from "../components/Content.vue";
 import Axios from "axios";
 import jump from "../plugins/jump";
 import config from "../plugins/config";
+
 export default {
   components: {
     PopCata,
     Pcontent,
     ReadSettings
   },
-  created() {
-    var config = JSON.parse(localStorage.getItem("config"));
-    if (config != null) this.$store.commit("setConfig", config);
-  },
-  beforeCreate() {
-    let config = JSON.parse(localStorage.getItem("config"));
-    if (config != null) this.$store.commit("setConfig", config);
-  },
-  mounted() {
-    this.loading = this.$loading({
-      target: this.$refs.content,
-      lock: true,
-      text: "正在获取内容",
-      spinner: "el-icon-loading",
-      background: "rgba(0,0,0,0)"
-    });
-    //获取书籍数据
-    const that = this;
-    let bookUrl = sessionStorage.getItem("bookUrl");
-    let bookName = sessionStorage.getItem("bookName");
-    let chapterIndex = sessionStorage.getItem("chapterIndex") || 0;
-    var book = JSON.parse(localStorage.getItem(bookUrl));
-    if (book == null || chapterIndex > 0) {
-      book = {
-        bookName: bookName,
-        bookUrl: bookUrl,
-        index: chapterIndex
-      };
-      localStorage.setItem(bookUrl, JSON.stringify(book));
-    }
-
-    // window.addEventListener keyup 声明函数
-
-    this.func_keyup = function (event) {
-      switch (event.key) {
-        case 'ArrowLeft':
-          event.stopPropagation();
-          event.preventDefault();
-          that.toLastChapter();
-          break;
-        case 'ArrowRight':
-          event.stopPropagation();
-          event.preventDefault();
-          that.toNextChapter();
-          break;
-        case 'ArrowUp':
-          event.stopPropagation();
-          event.preventDefault();
-          if (document.documentElement.scrollTop === 0) {
-            that.$message.warning("已到达页面顶部");
-          } else {
-            jump(0 - document.documentElement.clientHeight + 100);
-          }
-          break;
-        case 'ArrowDown':
-          event.stopPropagation();
-          event.preventDefault();
-          if (document.documentElement.clientHeight + document.documentElement.scrollTop === document.documentElement.scrollHeight) {
-            that.$message.warning("已到达页面底部");
-          } else {
-            jump(document.documentElement.clientHeight - 100);
-          }
-          break;
-      }
-    };
-
-    this.getCatalog(bookUrl).then(
-      res => {
-        let catalog = res.data.data;
-        book.catalog = catalog;
-        that.$store.commit("setReadingBook", book);
-        var index = that.$store.state.readingBook.index || 0;
-        this.getContent(index);
-        window.addEventListener('keyup', this.func_keyup);
-      },
-      err => {
-        that.loading.close();
-        that.$message.error("获取书籍目录失败");
-        throw err;
-      }
-    );
-  },
-  destroyed() {
-    window.removeEventListener('keyup', this.func_keyup);
-  },
-  watch: {
-    chapterName(to) {
-      this.title = to;
-    },
-    content() {
-      this.$store.commit("setContentLoading", false);
-    },
-    theme(theme) {
-      if (theme == 6) {
-        this.isNight = true;
-      } else {
-        this.isNight = false;
-      }
-    },
-    bodyColor(color) {
-      this.bodyTheme.background = color;
-    },
-    chapterColor(color) {
-      this.chapterTheme.background = color;
-    },
-    readWidth(width) {
-      this.chapterTheme.width = width;
-      let leftToolMargin = -((parseInt(width) + 130) / 2 + 68) + "px";
-      let rightToolMargin = -((parseInt(width) + 130) / 2 + 52) + "px";
-      this.leftBarTheme.marginLeft = leftToolMargin;
-      this.rightBarTheme.marginRight = rightToolMargin;
-    },
-    popupColor(color) {
-      this.leftBarTheme.background = color;
-      this.rightBarTheme.background = color;
-    },
-    readSettingsVisible(visible) {
-      if (!visible) {
-        let configText = JSON.stringify(this.$store.state.config);
-        localStorage.setItem("config", configText);
-      }
-    }
-  },
   data() {
     return {
+      domainDialogVisible: true, // 控制域名输入弹窗的显示
+      domain: "", // 用户输入的域名
       title: "",
       content: [],
       noPoint: true,
@@ -265,67 +165,124 @@ export default {
       }
     };
   },
-  computed: {
-    catalog() {
-      return this.$store.state.catalog;
-    },
-    windowHeight() {
-      return window.innerHeight;
-    },
-    contentHeight() {
-      return this.$refs.content.offsetHeight;
-    },
-    popCataVisible: {
-      get() {
-        return this.$store.state.popCataVisible;
-      },
-      set(value) {
-        this.$store.commit("setPopCataVisible", value);
-      }
-    },
-    readSettingsVisible: {
-      get() {
-        return this.$store.state.readSettingsVisible;
-      },
-      set(value) {
-        this.$store.commit("setReadSettingsVisible", value);
-      }
-    },
-    config() {
-      return this.$store.state.config;
-    },
-    theme() {
-      return this.config.theme;
-    },
-    bodyColor() {
-      return config.themes[this.config.theme].body;
-    },
-    chapterColor() {
-      return config.themes[this.config.theme].content;
-    },
-    popupColor() {
-      return config.themes[this.config.theme].popup;
-    },
-    readWidth() {
-      return this.$store.state.config.readWidth - 130 + "px";
-    },
-    cataWidth() {
-      return this.$store.state.config.readWidth - 33;
-    },
-    show() {
-      return this.$store.state.showContent;
-    }
-  },
   methods: {
-    getCatalog(bookUrl) {
-      return Axios.get(
-          "/getChapterList?url=" +
-          encodeURIComponent(bookUrl)
+    // 处理域名输入弹窗关闭
+    handleDomainDialogClose(done) {
+      if (!this.domain) {
+        this.$message.warning("请输入域名");
+        return;
+      }
+      done();
+    },
+    // 确认域名并加载内容
+    confirmDomain() {
+      //if (!this.domain) {
+      //  this.$message.warning("请输入域名");
+      //  return;
+      //}
+      this.domainDialogVisible = false; // 关闭弹窗
+      this.loadContent(); // 加载内容
+    },
+    close_domain()
+    {      this.domainDialogVisible = false; // Close the domain dialog
+      this.loadContent(); // Load the content
+    },
+    // 加载内容
+    loadContent() {
+      this.loading = this.$loading({
+        target: this.$refs.content,
+        lock: true,
+        text: "正在获取内容",
+        spinner: "el-icon-loading",
+        background: "rgba(0,0,0,0)"
+      });
+
+      // 从URL中获取书本ID
+      const bookId = this.getBookIdFromUrl();
+      if (!bookId) {
+        this.$message.error("未找到书本ID");
+        this.loading.close();
+        return;
+      }
+
+      // 获取书本目录
+      this.getCatalog(bookId).then(
+        res => {
+          let catalog = res.data;
+          console.log(catalog);
+          let book = {
+            bookId: bookId,
+            catalog: catalog
+          };
+          this.$store.commit("setReadingBook", book);
+
+          // 默认加载第一章
+          this.getContent(0);
+          window.addEventListener('keyup', this.func_keyup);
+        },
+        err => {
+          this.loading.close();
+          this.$message.error("获取书籍目录失败");
+          throw err;
+        }
       );
     },
+    // 从URL中获取书本ID
+    getBookIdFromUrl() {
+      const url = window.location.href;
+	  const index = url.indexOf('?');
+	  if (index !== -1) {
+		return decodeURIComponent(url.slice(index + 1));
+	  }
+    },
+    // 获取目录
+    getCatalog(bookId) {
+    console.log(`${bookId}/ChapterList.json`);
+  return Axios.get(`${bookId}/ChapterList.json`)
+    .then(res => {
+      // 处理未自动解析的情况
+      let rawData;
+      try {
+        rawData = typeof res.data === 'string' ? 
+          JSON.parse(res.data) : 
+          res.data;
+      } catch (e) {
+        console.error('JSON解析失败:', e);
+        throw new Error("无效的JSON格式");
+      }
+
+      // 调试输出实际数据结构
+      console.log('处理后的原始数据:', {
+        type: typeof rawData,
+        value: rawData
+      });
+
+      // 处理对象格式
+      if (typeof rawData === 'object' && !Array.isArray(rawData)) {
+        const catalogArray = Object.entries(rawData).map(([title, id]) => ({
+          title,
+          id: id // 确保转换为数字 //去你妈的转换尼玛呢
+        })).sort((a, b) => a.id - b.id); // 按ID排序
+        console.log("catalogArray",catalogArray);
+        return { data: catalogArray };
+      }
+
+      // 处理其他格式
+      throw new Error(`无法识别的数据结构: ${JSON.stringify(rawData)}`);
+    })
+    .catch(error => {
+      console.error('获取目录失败:', error);
+      throw error; // 保持错误传递
+    });
+},
+    // 获取章节内容
     getContent(index) {
-      //展示进度条
+    console.log('getContent',index);
       this.$store.commit("setShowContent", false);
+      
+      this.$store.commit("setShowContent", false);
+      
+      this.$store.state.readingBook.index=index;
       if (!this.loading.visible) {
         this.loading = this.$loading({
           target: this.$refs.content,
@@ -335,34 +292,25 @@ export default {
           background: "rgba(0,0,0,0)"
         });
       }
-      //保存阅读进度
-      let bookUrl = sessionStorage.getItem("bookUrl");
-      let book = JSON.parse(localStorage.getItem(bookUrl));
-      book.index = index;
-      localStorage.setItem(bookUrl, JSON.stringify(book));
-      this.$store.state.readingBook.index = index;
-      //let chapterUrl = this.$store.state.readingBook.catalog[index].url;
-      let chapterName = this.$store.state.readingBook.catalog[index].title;
-      let chapterIndex = this.$store.state.readingBook.catalog[index].index;
-      this.title = chapterName;
-      //强制滚回顶层
-      jump(this.$refs.top, { duration: 0 });
-      let that = this;
-      Axios.get(
-        "/getBookContent?url=" +
-          encodeURIComponent(bookUrl) + "&index=" + chapterIndex
-      ).then(
+
+      const bookId = this.getBookIdFromUrl();
+      const chapterId = this.$store.state.readingBook.catalog[index].id;
+	console.log(this.$store.state);
+      //Axios.get(`${bookId}/${chapterId}.json`).then(
+      console.log(`${bookId}/${chapterId}`);
+      Axios.get(`${bookId}/${chapterId}`).then(
         res => {
-          let data = res.data.data;
-          that.content = data.split(/\n+/);
+          let data = res.data; // 访问data字段
+          console.log(res);
+          this.content = data.split(/\n+/);
           this.$store.commit("setContentLoading", true);
-          that.loading.close();
-          that.noPoint = false;
-          that.$store.commit("setShowContent", true);
+          this.loading.close();
+          this.noPoint = false;
+          this.$store.commit("setShowContent", true);
         },
         err => {
-          that.$message.error("获取章节内容失败");
-          that.content = "　　获取章节内容失败！";
+          this.$message.error("获取章节内容失败");
+          this.content = "　　获取章节内容失败！";
           throw err;
         }
       );
@@ -377,6 +325,7 @@ export default {
       this.$store.commit("setContentLoading", true);
       let index = this.$store.state.readingBook.index;
       index++;
+      console.log(index,this.$store.state.readingBook.catalog[index])
       if (typeof this.$store.state.readingBook.catalog[index] !== "undefined") {
         this.$message.info("下一章");
         this.getContent(index);
@@ -403,13 +352,8 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
->>>.pop-setting {
-  margin-left: 68px;
-  top: 0;
-}
-
->>>.pop-cata {
-  margin-left: 10px;
+>>>.el-dialog__wrapper {
+  z-index: 9999 !important;
 }
 
 .chapter-wrapper {
